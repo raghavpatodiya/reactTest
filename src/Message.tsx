025f85de3350e750fccf0ed4c3d581cd0f249c15
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2023-12-25 12:42:57 +0530

    initial additions

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// pascal casing
+function Message() {
+    // jsx javascript xml
+    // return <h1>Hello World</h1>;
+    // try babel
+    const name = 'Raghav';
+    
+    return <h1>Hello {name}</h1>;
+
+}
+export default Message;
