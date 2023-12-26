fd84399c94782f5619c5908cfe124d0a86c20ad4
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2023-12-27 00:12:14 +0530

    components addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Message from './Message';
-
+// import Message from './Message';
+import ListGroup from "./components/ListGroup";
 function App(){
-  return <div><Message/></div>;
+  return <div><ListGroup/></div>;
 }
 export default App;
