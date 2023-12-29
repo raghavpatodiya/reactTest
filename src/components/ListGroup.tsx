a2eeef569be1fb5d5559957313ef9ac93ac9497f
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2023-12-29 12:54:59 +0530

    mapping in lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,16 @@
 // import { Fragment } from "react";
 //just use empty brackets, default taken as fragments
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     //react can't return 2 elements, so we wrap it all under div
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </>
   );
