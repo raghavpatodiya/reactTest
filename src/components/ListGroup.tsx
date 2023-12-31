92de26a9dca54c68008da236a637fe2367cde432
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2023-12-31 11:26:46 +0530

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,28 @@
 // import { Fragment } from "react";
 //just use empty brackets, default taken as fragments
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item Found</p>;
+  //     </>
+  //   );
 
   return (
     //react can't return 2 elements, so we wrap it all under div
+
+    //with braces we can render anything dynamically
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
