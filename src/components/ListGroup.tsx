1b350f07cf261ead69aee0af50edb590c000f77f
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-01-01 22:51:50 +0530

    Event Handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,9 @@
 // import { Fragment } from "react";
 //just use empty brackets, default taken as fragments
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  // items = [];
   // const message = items.length === 0 ? <p>No item found</p> : null;
   // const getMessage = () => {
   //   items.length === 0 ? <p>No item found</p> : null;
@@ -16,6 +17,8 @@ function ListGroup() {
   //     </>
   //   );
 
+  //event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     //react can't return 2 elements, so we wrap it all under div
 
@@ -26,7 +29,9 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
